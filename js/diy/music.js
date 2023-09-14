07ec0f5e0c54fbdd5a0eb494246b621d8849e0a9
System Administrator <root@WWF-MacBook-Pro.local>
2023-09-15 06:19:43 +0800

    Site updated: 2023-09-15 06:19:43

--- js/diy/music.js
+++ js/diy/music.js
@@ -9,9 +9,9 @@ const ap = new APlayer({
     listFolded: true,
     audio: [
     {
-        name: "左手指月",
-        artist: '萨顶顶',
-        url: 'http://m801.music.126.net/20230915053859/85dc131e45cae6e07a734d7b6a2969f7/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/15655489139/2358/b097/4bdb/12325723f16f5731cdf16f1825cb98b5.mp3',
+        name: "如愿",
+        artist: '王菲',
+        url: 'hhttps://cdn.jsdelivr.net/gh/baduyifei/tuchuang/ruyuan.mp3',
         cover: 'https://wenqidao.oss-cn-shanghai.aliyuncs.com/img/202309150523977.png', 
     },
     ]
