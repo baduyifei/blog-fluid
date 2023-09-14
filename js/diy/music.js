9b44b49aaa874399f420d574a51a2a341f3062c7
System Administrator <root@WWF-MacBook-Pro.local>
2023-09-15 06:30:19 +0800

    Site updated: 2023-09-15 06:30:19

--- js/diy/music.js
+++ js/diy/music.js
@@ -14,5 +14,11 @@ const ap = new APlayer({
         url: 'https://cdn.jsdelivr.net/gh/baduyifei/tuchuang/ruyuan.mp3',
         cover: 'https://wenqidao.oss-cn-shanghai.aliyuncs.com/img/202309150523977.png', 
     },
+    {
+        name: "左手指月",
+        artist: '萨顶顶',
+        url: 'https://cdn.jsdelivr.net/gh/baduyifei/tuchuang/zuoshouzhiyue.mp3',
+        cover: 'https://wenqidao.oss-cn-shanghai.aliyuncs.com/img/202309150523977.png', 
+    },
     ]
 });
