0d24a4bdbe40b944a52f6ebc02eb4983e5f77d24
System Administrator <root@WWF-MacBook-Pro.local>
2023-09-15 14:53:42 +0800

    Site updated: 2023-09-15 14:53:42

--- js/diy/music.js
+++ js/diy/music.js
@@ -3,7 +3,7 @@ const ap = new APlayer({
     fixed: true,
     autoplay: true,
     loop: 'all',
-    mini: false,
+    mini: true,
     theme: '#333',
     preload: 'auto',
     listFolded: true,
@@ -14,11 +14,11 @@ const ap = new APlayer({
         url: 'https://cdn.jsdelivr.net/gh/baduyifei/tuchuang/ruyuan.mp3',
         cover: 'https://wenqidao.oss-cn-shanghai.aliyuncs.com/img/202309150523977.png', 
     },
-    // {
-    //     name: "左手指月",
-    //     artist: '萨顶顶',
-    //     url: 'https://cdn.jsdelivr.net/gh/baduyifei/tuchuang/zuoshouzhiyue.mp3',
-    //     cover: 'https://wenqidao.oss-cn-shanghai.aliyuncs.com/img/202309150523977.png', 
-    // },
+    {
+        name: "左手指月",
+        artist: '萨顶顶',
+        url: 'https://cdn.jsdelivr.net/gh/baduyifei/tuchuang/zuoshouzhiyue.mp3',
+        cover: 'https://wenqidao.oss-cn-shanghai.aliyuncs.com/img/202309150523977.png', 
+    },
     ]
 });
