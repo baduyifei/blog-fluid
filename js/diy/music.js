d5f13b699f791ae839969288a8dcefce7e64a79a
System Administrator <root@WWF-MacBook-Pro.local>
2023-09-15 15:27:39 +0800

    Site updated: 2023-09-15 15:27:38

--- js/diy/music.js
+++ js/diy/music.js
@@ -20,5 +20,11 @@ const ap = new APlayer({
         url: 'https://cdn.jsdelivr.net/gh/baduyifei/tuchuang/zuoshouzhiyue.mp3',
         cover: 'https://wenqidao.oss-cn-shanghai.aliyuncs.com/img/202309150523977.png', 
     },
+    {
+        name: "小镇英雄青年",
+        artist: '王贰浪',
+        url: 'https://cdn.jsdelivr.net/gh/baduyifei/tuchuang/xiaozhenqingnian.mp3',
+        cover: 'https://wenqidao.oss-cn-shanghai.aliyuncs.com/img/202309150523977.png', 
+    },
     ]
 });
